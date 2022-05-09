0d24d6ae883afbe645143ee0e31befa741355cfc
Krishna Shrestha <krish_sth2005@yahoo.com>
2022-05-09 15:56:16 +1000

    front-end app initialized

---
+++ frontend-expenses-tracker/src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { Button } from "bootstrap";
+
+function App() {
+  return (
+    <div className="App">
+      <Button variant="primary">
+        <i class="fa-brands fa-font-awesome"></i>
+      </Button>
+    </div>
+  );
+}
+
+export default App;
