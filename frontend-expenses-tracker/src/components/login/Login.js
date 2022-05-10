061ba889145ad1fa86762dc8a80c1ce001ab5ba7
Krishna Shrestha <krish_sth2005@yahoo.com>
2022-05-10 14:43:04 +1000

    login

--- frontend-expenses-tracker/src/components/login/Login.js
+++ frontend-expenses-tracker/src/components/login/Login.js
@@ -1,6 +1,6 @@
 import React, { useState, useRef } from "react";
-import { Button, Form, Row } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Alert, Button, Form, Row, Spinner } from "react-bootstrap";
+import { Link, useNavigate } from "react-router-dom";
 import { postLogin } from "../helpers/axiosHelper";
 
 export const Login = () => {
@@ -8,6 +8,7 @@ export const Login = () => {
   const passwordRef = useRef("");
   const [error, setError] = useState("");
   const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
 
   const handleOnSubmit = async () => {
     const email = emailRef.current.value;
@@ -19,12 +20,15 @@ export const Login = () => {
     setLoading(true);
     const { data } = await postLogin({ email, password });
     setLoading(false);
-    console.log(data);
+    // console.log(data);
 
     if (data.status === "Success") {
-      //if log in success, store user data in session storage and redirect to dashboard
       const { name, email, _id } = data.user;
+      //if log in success, store user data in session storage and redirect to dashboard
       sessionStorage.setItem("user", JSON.stringify({ name, email, _id }));
+      setError("");
+      navigate("/dashboard");
+      return;
     }
 
     //else show error message
@@ -36,6 +40,10 @@ export const Login = () => {
       <Form>
         <h3>Welcome Back</h3>
         <hr />
+
+        {loading && <Spinner animation="border" variant="primary" />}
+        {error && <Alert variant="danger">{error}</Alert>}
+
         <Form.Group className="mb-3" controlId="formGroupEmail">
           <Form.Label>Email address</Form.Label>
           <Form.Control ref={emailRef} type="email" placeholder="Enter email" />
--- frontend-expenses-tracker/src/pages/Dashboard.js
+++ frontend-expenses-tracker/src/pages/Dashboard.js
@@ -1,6 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { MainLayout } from "../components/layout/MainLayout";
+import { useNavigate } from "react-router-dom";
 
 export const Dashboard = () => {
+  const navigate = useNavigate();
+  useEffect(() => {
+    const user = JSON.parse(sessionStorage.getItem("user"));
+
+    if (!user?._id) {
+      navigate("/");
+    }
+  }, []);
+
   return <MainLayout>Dashboard Page</MainLayout>;
 };
