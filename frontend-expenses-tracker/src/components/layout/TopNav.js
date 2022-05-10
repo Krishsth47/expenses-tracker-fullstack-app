8792a24701d67784eebc23d5894a22c6b3d65e2d
Krishna Shrestha <krish_sth2005@yahoo.com>
2022-05-10 15:02:28 +1000

    logout functionality

--- frontend-expenses-tracker/src/components/layout/TopNav.js
+++ frontend-expenses-tracker/src/components/layout/TopNav.js
@@ -1,21 +1,46 @@
-import React from "react";
-import { Navbar, Container, Nav } from "react-bootstrap";
+import React, { useEffect, useState } from "react";
+import { Navbar, Container, Nav, Button } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
+import { useNavigate } from "react-router-dom";
 
 export const TopNav = () => {
+  const navigate = useNavigate();
+  const [user, setUser] = useState({});
+
+  useEffect(() => {
+    const userInfo = JSON.parse(sessionStorage.getItem("user"));
+    setUser(userInfo);
+  }, []);
+
+  const handleOnLogout = () => {
+    sessionStorage.removeItem("user");
+    navigate("/");
+  };
+
   return (
     <Navbar bg="light" expand="md">
       <Container>
         <Navbar.Brand href="/">ET</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <LinkContainer to="/">
-              <Nav.Link>Login</Nav.Link>
-            </LinkContainer>
-            <LinkContainer to="/register">
-              <Nav.Link>Register</Nav.Link>
-            </LinkContainer>
+          <Nav className="ms-auto">
+            {user?._id ? (
+              <>
+                Welcome {user.name}
+                <Button variant="primary" onClick={handleOnLogout}>
+                  Logout
+                </Button>
+              </>
+            ) : (
+              <>
+                <LinkContainer to="/">
+                  <Nav.Link>Login</Nav.Link>
+                </LinkContainer>
+                <LinkContainer to="/register">
+                  <Nav.Link>Register</Nav.Link>
+                </LinkContainer>
+              </>
+            )}
           </Nav>
         </Navbar.Collapse>
       </Container>
--- frontend-expenses-tracker/src/pages/Dashboard.js
+++ frontend-expenses-tracker/src/pages/Dashboard.js
@@ -6,11 +6,10 @@ export const Dashboard = () => {
   const navigate = useNavigate();
   useEffect(() => {
     const user = JSON.parse(sessionStorage.getItem("user"));
-
     if (!user?._id) {
       navigate("/");
     }
-  }, []);
+  }, [navigate]);
 
   return <MainLayout>Dashboard Page</MainLayout>;
 };
